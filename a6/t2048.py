4c0fa8f6566ab7cfc4e81939408c2ccecdd854d2
orca1018 <dev@jiraroj.net>
2025-06-19 14:19:36 +0700

    setting up the project, I'll finish it tonight, hopefully

--- a6/t2048.py
+++ a6/t2048.py
@@ -1,8 +1,26 @@
-# Assignment XX, Task YY
-# Name: Eye Loveprograming
-# Collaborators: John Nonexistent
+# Assignment 6, Task 6
+# Name: Jiraroj Wiruchpongsanon
+# Collaborators: <NAME_1, NAME_2>
 # Time Spent: 4:00 hrs
+# Use of AI: NO
+# AI usage details: <DETAILS>
 # ----------------------------------
+
+"""for macos-arm64 user
+if your python or the venv is missing `_tkinter` -- should show sth like this:
+
+
+File "/opt/homebrew/Cellar/python@3.13/3.13.3/Frameworks/Python.framework/Versions/3.13/lib/python3.13/tkinter/__init__.py", line 38, in <module>
+    import _tkinter # If this fails your Python may not be configured for Tk
+    ^^^^^^^^^^^^^^^
+ModuleNotFoundError: No module named '_tkinter'
+
+
+I supposed that means, you install python via homebrew, and homebrew doesn't have Tk support.
+Thus, you need to install tk for python separately via `brew install python-tk`
+"""
+
+### Your code start here ######
 from typing import List, Tuple
 
 Board = List[List[str]]
@@ -15,6 +33,7 @@ Board = List[List[str]]
 def isGameOver(board: Board) -> bool:
     return False
 
+
 # Returns a tuple (changed, new_board)
 # where:
 #  changed - a boolean indicating if
@@ -26,6 +45,7 @@ def isGameOver(board: Board) -> bool:
 def doKeyUp(board: Board) -> Tuple[bool, Board]:
     return False, board
 
+
 # Returns a tuple (changed, new_board)
 # where:
 #  changed - a boolean indicating if
@@ -36,6 +56,7 @@ def doKeyUp(board: Board) -> Tuple[bool, Board]:
 
 def doKeyDown(board: Board) -> Tuple[bool, Board]:
     return False, board
+
 
 # Returns a tuple (changed, new_board)
 # where:
