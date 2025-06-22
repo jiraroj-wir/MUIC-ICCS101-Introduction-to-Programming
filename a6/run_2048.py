c217fba453612df326609d8e4d1718e5a5363441
orca1018 <dev@jiraroj.net>
2025-06-22 18:37:21 +0700

    it runs fine now, but theres a bug that made the program crash when played for a while, I'll try to fix that. Might add comments on the code as well

--- a6/run_2048.py
+++ a6/run_2048.py
@@ -7,23 +7,24 @@ import random
 from tkinter import messagebox as tkMessageBox
 import t2048
 
+
 class Tile(Canvas):
-    """ UI for each tile """
+    """UI for each tile"""
+
     def __init__(self, master, width=0, height=0, **kwargs):
         self.width = width
         self.height = height
         Canvas.__init__(self, master, width=self.width, height=self.height)
         self.create_rectangle(0, 0, width, height, width=5)
-        self.textObj = self.create_text(width/2, 
-                                        height/2, 
-                                        font=kwargs['font'],
-                                        text=" ")
+        self.textObj = self.create_text(
+            width / 2, height / 2, font=kwargs["font"], text=" "
+        )
 
     def setText(self, s):
         self.itemconfigure(self.textObj, text=s)
 
     def getText(self):
-        return self.itemcget(self.textObj, 'text')
+        return self.itemcget(self.textObj, "text")
 
     def pack(self, *args, **kwargs):
         Canvas.pack(self, *args, **kwargs)
@@ -32,6 +33,7 @@ class Tile(Canvas):
     def grid(self, *args, **kwargs):
         Canvas.grid(self, *args, **kwargs)
         self.grid_propagate(False)
+
 
 class Game2048:
     def __init__(self, master, num_rows, num_cols):
@@ -43,19 +45,16 @@ class Game2048:
         h = int(root.winfo_height() / num_rows)
         for i in range(num_rows):
             for j in range(num_cols):
-                self.tiles[(i,j)] = Tile(master,
-                                         width=w,
-                                         height=h,
-                                         padx=40,
-                                         pady=40,
-                                         font=("Helvetica", 32))
-                self.tiles[(i,j)].grid(row=i, column=j)
+                self.tiles[(i, j)] = Tile(
+                    master, width=w, height=h, padx=40, pady=40, font=("Helvetica", 32)
+                )
+                self.tiles[(i, j)].grid(row=i, column=j)
         self.reset()
 
     def _setBoard(self, board):
         for i in range(len(board)):
             for j in range(len(board[0])):
-                self.tiles[(i,j)].setText(board[i][j])
+                self.tiles[(i, j)].setText(board[i][j])
         self.master.update()
 
     def _getBoard(self):
@@ -63,24 +62,26 @@ class Game2048:
         for i in range(self.num_rows):
             temp = []
             for j in range(self.num_cols):
-                temp.append(self.tiles[(i,j)].getText())
+                temp.append(self.tiles[(i, j)].getText())
             board.append(temp)
         return board
 
     def _gen_number(self, board):
-        ''' Generates a new number. 
+        """Generates a new number.
         If there is still a space on the board, return
-          { 
+          {
             num: the new number,
             row: row number,
             col: col number
-          } 
-        Otherwise, return None 
-        '''
-        num = random.randint(0,10)
+          }
+        Otherwise, return None
+        """
+        num = random.randint(0, 10)
         # Custom distribution
-        if num < 8: num = 2
-        else: num = 4
+        if num < 8:
+            num = 2
+        else:
+            num = 4
         # find an empty space to spawn
         potential = t2048.emptyPos(board)
         # no more move
@@ -89,53 +90,56 @@ class Game2048:
             return None
         # sample a location from the potential positions
         loc = random.choice(potential)
-        return {'num': str(num), 'row': loc[0], 'col': loc[1]}
+        return {"num": str(num), "row": loc[0], "col": loc[1]}
 
     def reset(self):
         # create an empty board
         board = []
         for i in range(self.num_rows):
-            board.append([' '] * self.num_cols)
+            board.append([" "] * self.num_cols)
         # add one number
         c = self._gen_number(board)
         if c is not None:
-            board[c['row']][c['col']] = c['num']
+            board[c["row"]][c["col"]] = c["num"]
         self._setBoard(board)
 
     def keypress(self, event):
         k = event.char.lower()
-        if k in ['w','a','s','d']:
+        if k in ["w", "a", "s", "d", "h", "j", "k", "l"]:  # add vim keybinding
             board = self._getBoard()
-            if k == 'w': 
+            if k == "w" or k == "k":
                 changed, board = t2048.doKeyUp(board)
-            elif k == 'a':
+            elif k == "a" or k == "h":
                 changed, board = t2048.doKeyLeft(board)
-            elif k == 's':
+            elif k == "s" or k == "j":
                 changed, board = t2048.doKeyDown(board)
-            elif k == 'd':
+            elif k == "d" or k == "l":
                 changed, board = t2048.doKeyRight(board)
             if changed:
                 c = self._gen_number(board)
                 # Add new number if possible
                 if c is not None:
-                    board[c['row']][c['col']] = c['num']
+                    board[c["row"]][c["col"]] = c["num"]
                     self._setBoard(board)
             # Check game over
             if t2048.isGameOver(board):
                 self.gameOver()
-        elif k == 'r':
+        elif k == "r":
             self.reset()
 
     def gameOver(self):
         tkMessageBox.showinfo("Game Over", "Game Over!")
         self.reset()
 
+
 #####
 
 root = Tk()
 
 # Main UI config
-root.title = "2048"
+
+# root.title = "2048"
+root.title("2048")  # fix
 root.minsize(width=400, height=400)
 root.resizable(width=False, height=False)
 root.update()
--- a6/t2048.py
+++ a6/t2048.py
@@ -29,9 +29,39 @@ Board = List[List[str]]
 # possible move left. If no more moves,
 # return True. Otherwise return False.
 
+DIRECTION = {
+    "UP": (-1, 0),
+    "DOWN": (1, 0),
+    "LEFT": (0, -1),
+    "RIGHT": (0, 1),
+}
+
+
+# https://www.geeksforgeeks.org/matrix-transpose-without-numpy-in-python/
+def transpose(board: Board) -> Board:
+    return [list(row) for row in zip(*board)]
+
+
+# horizontal reflection
+def mirror(board: Board) -> Board:
+    reflection = []
+    for row in board:
+        new_row = row[:]
+        new_row.reverse()
+        reflection.append(new_row)
+    return reflection
+
 
 def isGameOver(board: Board) -> bool:
-    return False
+    if emptyPos(board):
+        return False
+
+    change_U, _board = doKeyUp(board)
+    change_D, _board = doKeyDown(board)
+    change_R, _board = doKeyRight(board)
+    change_L, _board = doKeyLeft(board)
+
+    return not (change_U or change_D or change_R or change_L)
 
 
 # Returns a tuple (changed, new_board)
@@ -43,7 +73,11 @@ def isGameOver(board: Board) -> bool:
 
 
 def doKeyUp(board: Board) -> Tuple[bool, Board]:
-    return False, board
+    board = mirror(transpose(board))
+    [changed, new_board] = doKeyRight(board)
+    new_board = transpose(mirror(new_board))
+
+    return changed, new_board
 
 
 # Returns a tuple (changed, new_board)
@@ -55,7 +89,11 @@ def doKeyUp(board: Board) -> Tuple[bool, Board]:
 
 
 def doKeyDown(board: Board) -> Tuple[bool, Board]:
-    return False, board
+    board = transpose(board)
+    [changed, new_board] = doKeyRight(board)
+    new_board = transpose(new_board)
+
+    return changed, new_board
 
 
 # Returns a tuple (changed, new_board)
@@ -67,7 +105,11 @@ def doKeyDown(board: Board) -> Tuple[bool, Board]:
 
 
 def doKeyLeft(board: Board) -> Tuple[bool, Board]:
-    return False, board
+    reflection = mirror(board)
+    [changed, new_board] = doKeyRight(reflection)
+    new_board = mirror(new_board)
+
+    return changed, new_board
 
 
 # Returns a tuple (changed, new_board)
@@ -76,11 +118,58 @@ def doKeyLeft(board: Board) -> Tuple[bool, Board]:
 #            the board has changed.
 #  new_board - the board after the user
 #              presses the 'Right' key.
+#
+# This is so ugly, I can't make it better -- sadly
 def doKeyRight(board: Board) -> Tuple[bool, Board]:
-    return False, board
+    new_board = []
+    changed = False
+    for row in board:
+        # STEP 1: Slide in the Direction of Movement
+        new_row = []
+        temp = []
+        for mem in row:
+            if mem == " ":
+                temp.append(" ")
+                continue
+            new_row.append(mem)
+
+        new_row = temp + new_row
+
+        # STEP 2: Compact Similar Blocks
+        idx = len(new_row) - 1
+        while idx > 0:
+            if new_row[idx] != " " and new_row[idx] == new_row[idx - 1]:
+                new_row[idx] = str(int(new_row[idx]) + int(new_row[idx - 1]))
+                new_row[idx - 1] = " "
+                idx -= 1
+            idx -= 1
+
+        # STEP 3: Slide Once More
+        new_new_row = []
+        temp = []
+        for mem in new_row:
+            if mem == " ":
+                temp.append(" ")
+                continue
+            new_new_row.append(mem)
+
+        new_new_row = temp + new_new_row
+
+        new_board.append(new_new_row)
+
+        if new_new_row != row:
+            changed = True
+
+    return changed, new_board
 
 
 # Returns a list of tuples (row, col)
 # indicating where the empty spots are
 def emptyPos(board: Board) -> List[Tuple[int, int]]:
-    return []
+    empty_position = []
+    for idx_i in range(len(board)):
+        for idx_j in range(len(board[idx_i])):
+            if board[idx_i][idx_j] == " ":
+                empty_position.append((idx_i, idx_j))
+
+    return empty_position
