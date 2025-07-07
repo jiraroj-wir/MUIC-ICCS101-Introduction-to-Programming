9c1793d2dc32948ae8980498dde604075f1d4c76
orca1018 <dev@jiraroj.net>
2025-07-07 12:03:39 +0700

    task 2, done

--- a7/cards.py
+++ a7/cards.py
@@ -1,9 +1,9 @@
 # Assignment 7, Task 2
 # Name: Jiraroj Wiruchpongsanon
 # Collaborators: <NAME_1, NAME_2>
-# Time Spent: 0.3 hrs
-# Use of AI: NO
-# AI usage details: <DETAILS>
+# Time Spent: 4 hrs
+# Use of AI: YES
+# AI usage details: Fix some typo, clarify the time complexity and emotional support
 
 
 ### Your code start here ######
@@ -192,6 +192,8 @@ def all_hands() -> list[Hand]:
 
         all.append(hand)
 
+        # this is equivalent to another 4 loops anyway, whats the point?
+        # I bet this is even slower than 5 layers of for loop
         e += 1
         if e >= deck_len:
             d += 1
@@ -209,14 +211,18 @@ def all_hands() -> list[Hand]:
     return all
 
 
-assert len(all_hands()) == 2598960
+# comment this out for submission, this took 6 seconds
+# assert len(all_hands()) == 2598960
 
 """I dont know whether the assignment wanted a new solution for all_straight_flush(), 
 all_four_of_a_kind(), all_full_house() and all_two_pair(); or just use the existing functions. 
 I'll do both.
+
+if use the existing functions; running all the functions will takes over 90 seconds
 """
 
 
+"""
 def all_straight_flush() -> list[Hand]:
     hands = all_hands()
     all_possible_stright_flush = []
@@ -224,8 +230,190 @@ def all_straight_flush() -> list[Hand]:
         if is_straight_flush(mem):
             all_possible_stright_flush.append(mem)
 
-    print(len(all_possible_stright_flush))
+    return all_possible_stright_flush
+"""
+
+
+def all_straight_flush() -> list[Hand]:
+    suit_list = ["Club", "Diamond", "Heart", "Spade"]
+    rank_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+    all_possible_stright_flush = []
+    for idx in range(len(rank_list) - 4):
+        for suit in suit_list:
+            all_possible_stright_flush.append(
+                {
+                    (suit, rank_list[idx]),
+                    (suit, rank_list[idx + 1]),
+                    (suit, rank_list[idx + 2]),
+                    (suit, rank_list[idx + 3]),
+                    (suit, rank_list[idx + 4]),
+                }
+            )
     return all_possible_stright_flush
 
 
 assert len(all_straight_flush()) == 40
+
+
+"""
+def all_four_of_a_kind() -> list[Hand]:
+    hands = all_hands()
+    all_possible_four_of_a_kind = []
+    for mem in hands:
+        if is_four_of_a_kind(mem):
+            all_possible_four_of_a_kind.append(mem)
+
+    return all_possible_four_of_a_kind
+"""
+
+
+def all_four_of_a_kind() -> list[Hand]:
+    suit_list = ["Club", "Diamond", "Heart", "Spade"]
+    rank_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    deck = []
+    for suit in suit_list:
+        for rank in rank_list:
+            deck.append((suit, rank))
+
+    all_possible_four_of_a_kind = []
+    for rank in rank_list:
+        for mem in deck:
+            if mem[1] != rank:
+                all_possible_four_of_a_kind.append(
+                    {
+                        (suit_list[0], rank),
+                        (suit_list[1], rank),
+                        (suit_list[2], rank),
+                        (suit_list[3], rank),
+                        mem,
+                    }
+                )
+
+    return all_possible_four_of_a_kind
+
+
+assert len(all_four_of_a_kind()) == 624
+
+
+"""
+def all_full_hose() -> list[Hand]:
+    hands = all_hands()
+    all_possible_full_house = []
+    for mem in hands:
+        if is_full_house(mem):
+            all_possible_full_house.append(mem)
+
+    return all_possible_full_house
+"""
+
+
+def all_full_house() -> list[Hand]:
+    suit_list = ["Club", "Diamond", "Heart", "Spade"]
+    rank_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    # combination of suit: C(4, 3)
+    # small list -- precompute, is this allowed?
+    c_4_3 = [
+        ["Club", "Diamond", "Heart"],
+        ["Club", "Diamond", "Spade"],
+        ["Club", "Heart", "Spade"],
+        ["Diamond", "Heart", "Spade"],
+    ]
+    # combination of suit: C(4, 2)
+    c_4_2 = []
+    for i in range(len(suit_list)):
+        for j in range(i + 1, len(suit_list)):
+            c_4_2.append([suit_list[i], suit_list[j]])
+
+    all_possible_full_house = []
+    for r1_idx in range(len(rank_list)):
+        for r2_idx in range(len(rank_list)):
+            if r1_idx == r2_idx:
+                continue
+
+            for three_of_a_kind in c_4_3:
+                hand = []
+                for suit in three_of_a_kind:
+                    hand.append((suit, rank_list[r1_idx]))
+
+                for pair in c_4_2:
+                    full_hand = set()
+                    for card in hand:
+                        full_hand.add(card)
+                    full_hand.add((pair[0], rank_list[r2_idx]))
+                    full_hand.add((pair[1], rank_list[r2_idx]))
+                    all_possible_full_house.append(full_hand)
+
+    return all_possible_full_house
+
+
+assert len(all_full_house()) == 3744
+
+
+"""
+def all_two_pair() -> list[Hand]:
+    hands = all_hands()
+    all_possible_two_pair = []
+    for mem in hands:
+        if is_two_pair(mem):
+            all_possible_two_pair.append(mem)
+
+    return all_possible_two_pair
+"""
+
+
+def all_two_pair() -> list[Hand]:
+    suit_list = ["Club", "Diamond", "Heart", "Spade"]
+    rank_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    deck = []
+    for suit in suit_list:
+        for rank in rank_list:
+            deck.append((suit, rank))
+
+    # combination of suit: C(4, 3)
+    # small list -- precompute, is this allowed?
+    """
+    c_4_3 = [
+        ["Club", "Diamond", "Heart"],
+        ["Club", "Diamond", "Spade"],
+        ["Club", "Heart", "Spade"],
+        ["Diamond", "Heart", "Spade"],
+    ]
+    """
+    # combination of suit: C(4, 2)
+    c_4_2 = []
+    for i in range(len(suit_list)):
+        for j in range(i + 1, len(suit_list)):
+            c_4_2.append([suit_list[i], suit_list[j]])
+
+    all_possible_two_pair = []
+    for r1_idx in range(len(rank_list)):
+        for r2_idx in range(r1_idx + 1, len(rank_list)):
+            for pair_1 in c_4_2:
+                hand_1 = []
+                for suit in pair_1:
+                    hand_1.append((suit, rank_list[r1_idx]))
+
+                for pair_2 in c_4_2:
+                    hand_2 = set()
+                    for clone_1 in hand_1:
+                        hand_2.add(clone_1)
+                    hand_2.add((pair_2[0], rank_list[r2_idx]))
+                    hand_2.add((pair_2[1], rank_list[r2_idx]))
+
+                    for card in deck:
+                        if card[1] == rank_list[r1_idx] or card[1] == rank_list[r2_idx]:
+                            continue
+
+                        full_hand = set()
+                        for clone_2 in hand_2:
+                            full_hand.add(clone_2)
+                        full_hand.add(card)
+
+                        all_possible_two_pair.append(full_hand)
+
+    return all_possible_two_pair
+
+
+assert len(all_two_pair()) == 123552
